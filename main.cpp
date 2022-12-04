f36284584fe8b504af97f63d0b7acbfe4c79c9ba
Maksim0202 <119806509+Maksim0202@users.noreply.github.com>
2022-12-05 00:10:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 double my_pow(double num, int deg)
 {
 	int res = num;
-	for (int i = 1; i < deg; i++)
+	for (int i = 1; i < abs(deg); i++)
 	{
 		res *= num;
+	}
+	if(deg < 0)
+	{
+		res = 1 / res;
 	}
 	return res;
 }
